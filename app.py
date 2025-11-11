5ccc77996e6a6b57015ce6409277d425c4bb34ae
TheTrueFalse <matheuseback@gmail.com>
2025-11-10 21:44:12 -0300

    integrado com o banco

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
 DB_USER = "neondb_owner"
 DB_PASSWORD = "npg_7qPbIwLmQ1Nf"
 DB_HOST = "ep-young-field-a89tdevs-pooler.eastus2.azure.neon.tech"
 DB_PORT = "5432"
 DB_NAME = "neondb"
 
-app = Flask(__name__)
+DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
